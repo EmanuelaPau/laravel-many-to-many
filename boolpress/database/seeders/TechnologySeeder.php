750219e7b6afa989831b77a963bf63f9ffd60fe2
EmanuelaPau <uapame@gmail.com>
2023-08-28 17:27:47 +0200

    Add seeder

--- boolpress/database/seeders/TechnologySeeder.php
+++ boolpress/database/seeders/TechnologySeeder.php
@@ -2,16 +2,35 @@
 
 namespace Database\Seeders;
 
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class TechnologySeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
+        $technologies = [
+            'Laravel',
+            'Html',
+            'Javascript',
+            'CSS',
+            'SCSS',
+            'Vite',
+            'Php',
+            'MAMP',
+        ];
+
+        foreach ($technologies as $technology) {
+            $newTechnology = new Technology();
+            $newTechnology->name = $technology;
+            $newTechnology->color = $faker->hexColor();
+            $newTechnology->save();
+        }
         //
     }
 }
